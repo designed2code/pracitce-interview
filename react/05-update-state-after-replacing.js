7ee7ff44bd2249e08a6edf072e016cc597591738
Sachin <sachin@Sachins-MacBook-Air.local>
2025-07-21 16:03:51 +0530

    update state after replacing

---
+++ react/05-update-state-after-replacing.js
@@ -0,0 +1,20 @@
+// What about this event handler? What do you think number will be in the next render?
+import { useState } from "react";
+
+export default function Counter() {
+  const [number, setNumber] = useState(0);
+
+  return (
+    <>
+      <h1>{number}</h1>
+      <button
+        onClick={() => {
+          setNumber(number + 5);
+          setNumber((n) => n + 1);
+        }}
+      >
+        Increase the number
+      </button>
+    </>
+  );
+}
