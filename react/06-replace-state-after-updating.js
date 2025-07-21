5e42a75c7b32f417c58b056df6099a326ec0cfd9
Sachin <sachin@Sachins-MacBook-Air.local>
2025-07-21 16:09:20 +0530

    replace state after update snippet

---
+++ react/06-replace-state-after-updating.js
@@ -0,0 +1,21 @@
+// What do you think number will be in the next render?
+import { useState } from "react";
+
+export default function Counter() {
+  const [number, setNumber] = useState(0);
+
+  return (
+    <>
+      <h1>{number}</h1>
+      <button
+        onClick={() => {
+          setNumber(number + 5);
+          setNumber((n) => n + 1);
+          setNumber(42);
+        }}
+      >
+        Increase the number
+      </button>
+    </>
+  );
+}
