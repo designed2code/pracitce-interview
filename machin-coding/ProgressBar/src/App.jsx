937759f0d2bc64c22db622d370549cb7d5f89666
Sachin <sachin@Sachins-MacBook-Air.local>
2025-07-21 20:44:35 +0530

    machine coding question

---
+++ machin-coding/ProgressBar/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import { ProgressBar1 } from "./components/ProgressBar1";
+function App() {
+  return (
+    <>
+      <ProgressBar1 />
+    </>
+  );
+}
+
+export default App;
---
+++ machin-coding/ProgressBar/src/components/ProgressBar1.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const ProgressBar1 = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
+};
