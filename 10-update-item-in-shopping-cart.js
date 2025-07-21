65c37c3d22832e9e2d83ea2682c04ceba2bae3e9
Sachin <sachin@Sachins-MacBook-Air.local>
2025-07-21 18:25:43 +0530

    added practice links

---
+++ 10-update-item-in-shopping-cart.js
@@ -0,0 +1,5 @@
+// https://react.dev/learn/updating-arrays-in-state#update-an-item-in-the-shopping-cart
+
+// https://react.dev/learn/updating-arrays-in-state#remove-an-item-from-the-shopping-cart
+
+// https://react.dev/learn/updating-arrays-in-state#fix-the-mutations-using-non-mutative-methods
