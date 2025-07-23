4a51f4ec6e50462a40fe7ffc763e4a97ee188610
Sachin <sachin@Sachins-MacBook-Air.local>
2025-07-24 01:10:28 +0530

    two-sum-sorted https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/?envType=study-plan-v2&envId=top-interview-150

---
+++ leetcode-150/lc-167-two-sum-II.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/?envType=study-plan-v2&envId=top-interview-150
+
+// Tricky Variation on GFG :- https://www.geeksforgeeks.org/problems/pair-with-given-sum-in-a-sorted-array4940/1
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+  let left = 0;
+  let right = numbers.length - 1;
+  while (left < right) {
+    if (numbers[left] + numbers[right] == target) {
+      return [left + 1, right + 1];
+    } else if (numbers[left] + numbers[right] > target) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+};
