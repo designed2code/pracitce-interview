acba5fe6390dc7e54ea49a781a76ce93c191e392
Sachin <sachin@Sachins-MacBook-Air.local>
2025-08-16 13:49:56 +0530

    Selection sort https://www.youtube.com/watch?v=EU9FIt1t-Is

---
+++ sorting_algos/selectionSort.js
@@ -0,0 +1,13 @@
+// Refer https://www.youtube.com/watch?v=EU9FIt1t-Is
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+  }
+  return arr;
+}
