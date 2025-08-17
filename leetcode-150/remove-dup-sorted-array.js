7ac24f7b661aa21d1cdb991db2185f7fd568a701
Sachin <sachin@Sachins-MacBook-Air.local>
2025-08-17 21:06:43 +0530

    updated solution on remove dup from sorted arr

--- leetcode-150/remove-dup-sorted-array.js
+++ leetcode-150/remove-dup-sorted-array.js
@@ -1,24 +1,18 @@
+// Refer https://www.youtube.com/watch?v=06ALbFrgIoQ
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-  let count = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== nums[i - 1]) {
-      nums[count] = nums[i];
-      count++;
+  let i = 0;
+  let j = 1;
+  let n = nums.length;
+  while (j < n) {
+    if (nums[j] !== nums[i]) {
+      i++;
+      nums[i] = nums[j];
     }
+    j++;
   }
-  return count;
+  return i + 1;
 };
-
-/*
-count tracks where to write the next new value.
-
-Whenever the current value nums[i] differs from the previous value (nums[i-1]), it’s the first time we’ve seen that number—so we copy it into nums[count] and bump count.
-
-If it’s the same as nums[i-1], we already have it in our compacted prefix, so we skip it.
-
-At the end, count is exactly how many unique values we kept, and the first count entries of nums hold them in order.
-*/
