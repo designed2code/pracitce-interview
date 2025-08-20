7ed33b83f2ef1b5e3dacfc3cc87c6eaac3e108e2
Sachin <sachin@Sachins-MacBook-Air.local>
2025-08-20 21:24:05 +0530

    binary search

---
+++ searching/binarySearch.js
@@ -0,0 +1,17 @@
+function binarySearch(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  let result = -1;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] == target) {
+      result = mid;
+      right = mid - 1;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return result;
+}
