6ca46bd392d4f5337c4a56e4e2b5f9ffd4a83fe8
Sachin <sachin@Sachins-MacBook-Air.local>
2025-08-27 14:29:17 +0530

    check ith bit

---
+++ bit-manipulation/02-check-ith-bit.js
@@ -0,0 +1,11 @@
+function checkIthBit(n, k) {
+  if ((1 & (n >> k)) !== 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(checkIthBit(4, 0));
+console.log(checkIthBit(4, 2));
+console.log(checkIthBit(500, 3));
