0a8b79a58c7d23e4cf8089c3391769e14e259ebe
Sachin <sachin@Sachins-MacBook-Air.local>
2025-08-27 14:34:29 +0530

    checkIthBitLeftShift

--- bit-manipulation/02-check-ith-bit.js
+++ bit-manipulation/02-check-ith-bit.js
@@ -9,3 +9,11 @@ function checkIthBit(n, k) {
 console.log(checkIthBit(4, 0));
 console.log(checkIthBit(4, 2));
 console.log(checkIthBit(500, 3));
+
+function checkIthBitLeftShift(n, k) {
+  if ((n & (1 << k)) !== 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
